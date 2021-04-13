202377426e7493033159ed94129b5ac48bd49926
Olexander <slozhna0@gmail.com>
2021-04-13 17:12:52 +0500

    React bootstrap setup & header, footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
-function App() {
-    return <div>Aboba</div>;
-}
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <h1>Aboba</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
+};
 
 export default App;
