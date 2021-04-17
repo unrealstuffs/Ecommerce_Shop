8d3c94bd139c279161c7c5e86a0962bcc044a312
Olexander <slozhna0@gmail.com>
2021-04-17 16:05:32 +0500

    Cart screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { CART_ADD_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    const { data } = await axois.get(`/api/products/${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
 
     dispatch({
         type: CART_ADD_ITEM,
