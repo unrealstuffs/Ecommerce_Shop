a66cd53f0f50b7cfcb3f0ca389d5ca938831995b
Olexander <slozhna0@gmail.com>
2021-04-19 19:44:29 +0500

    User login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -15,9 +16,10 @@ const App = () => {
             <Header />
             <main className="py-5">
                 <Container>
-                    <Route path="/" component={HomeScreen} exact />
+                    <Route path="/login" component={LoginScreen} />
                     <Route path="/product/:id" component={ProductScreen} />
                     <Route path="/cart/:id?" component={CartScreen} />
+                    <Route path="/" component={HomeScreen} exact />
                 </Container>
             </main>
             <Footer />
